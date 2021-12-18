5145496e3c90be7a72788130e4a0d7e70e01711b
Sujauddin Sekh <sujazoya@gmail.com>
2021-12-18 18:17:13 +0530

    thi
    th

---
+++ Assets/My_Assets/Unity_HoleVsColors-master/Levels/LevelItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelItem : MonoBehaviour
+{
+    public int[] itemIndex;
+    public int[] powerupsIndex;
+    public int[] blastIndex;
+}
